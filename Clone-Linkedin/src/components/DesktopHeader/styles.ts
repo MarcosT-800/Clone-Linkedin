a7305f37319af8268c6e56cc4a9b6c9e5587c6e3
Marcos Alves <82832426+MarcosT-800@users.noreply.github.com>
2022-04-21 15:37:34 -0300

    Update styles.ts

--- Clone-Linkedin/src/components/DesktopHeader/styles.ts
+++ Clone-Linkedin/src/components/DesktopHeader/styles.ts
@@ -3,32 +3,33 @@ import { GrLinkedin } from 'react-icons/gr';
 import { AiFillHome, AiOutlineBell, AiFillCaretDown } from 'react-icons/ai';
 
 export const Container = styled.div`
-  background: var(--color-header);
+  background: var(--color-header);//cor de fundo do header
   padding: 0 30px;
 
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
-  z-index: 2;
+  z-index: 2; //passará por cima de todo elemento da tela
 
   display: none;
-
+  
+//só aparecerá quando a tela for 1180px
   @media (min-width: 1180px) {
     display: block;
   }
 `;
 
 export const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex: 1;
+  display: flex; //deixa um do lado do outro
+  justify-content: space-between; //joga o inten lá longe
+  flex: 1;// consumirá o maximo de espaço que ele conseguir
 
   max-width: 1128px;
   margin: 0 auto;
-  height: 52px;
+  height: 52px; //altura
 
-  .left,
+  .left, 
   .right nav {
     display: flex;
     align-items: center;
@@ -38,7 +39,7 @@ export const Wrapper = styled.div`
     height: 100%;
 
     button {
-      background: none;
+      background: none; //sem cor de fundo
       border: 0;
       outline: 0;
 
@@ -46,15 +47,15 @@ export const Wrapper = styled.div`
       flex-direction: column;
       align-items: center;
       justify-content: center;
-      min-width: 90px;
+      min-width: 90px; //espaço entre os botões
       min-height: 100%;
 
       color: var(--color-icons);
-      cursor: pointer;
-      &:hover {
+      cursor: pointer; //deixa uma mãozinha
+      &:hover { //ao passar o mouse a cor do icone ficará branca
         color: var(--color-white);
       }
-      &.active {
+      &.active { //o botão que for ativo ficará na cor branca
         border-bottom: 2px solid var(--color-white);
       }
     }
@@ -62,12 +63,12 @@ export const Wrapper = styled.div`
 `;
 
 export const LinkedInIcon = styled(GrLinkedin)`
-  height: 34px;
-  width: 34px;
+  height: 34px; //altura
+  width: 34px; //largura
   color: var(--color-linkedin);
   background-color: #fff;
-  border-radius: 4px;
-  flex-shrink: 0;
+  border-radius: 4px; //borda redonda
+  flex-shrink: 0; //não fica amassado com tela pequena
 `;
 
 export const SearchInput = styled.input`
@@ -81,11 +82,12 @@ export const SearchInput = styled.input`
   outline: none;
   border-radius: 2px;
 
-  &:focus {
+  &:focus { //quando alguém clicar nele ele ficará branco
     background: var(--color-white);
   }
 `;
 
+//css aplicado para todos os icones
 const generalIconCSS = css`
   width: 24px;
   height: 24px;
@@ -102,7 +104,7 @@ export const NotificationsIcon = styled(AiOutlineBell)`
 export const ProfileCircle = styled.img`
   width: 24px;
   height: 24px;
-  border-radius: 50%;
+  border-radius: 50%; //deixa arredondado
   border: 1px solid var(--color-icons);
 `;
 
